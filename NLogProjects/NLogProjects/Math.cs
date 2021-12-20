d60dba2e7d15f72c23fd247880c5200dcde0f199
Rajesh T k <rajeshmadan2017@gmail.com>
2021-12-20 19:43:56 +0530

    nlog code

---
+++ NLogProjects/NLogProjects/Math.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NLogProjects
+{
+    public class Math
+    {
+        Nlog nlog = new Nlog();
+        public  double DivideTwoNumbers(int a ,int b)
+        {
+            try
+            {
+                nlog.Information("Entered inside DivideTwoNumbers");
+                var value= a / b;
+
+                nlog.Information("result of a/b= "+value);
+                return value;
+            }
+            catch (Exception ex)
+            {
+                nlog.Error(ex.Message);
+            }
+            return -1;
+        }
+    }
+}
---
+++ NLogProjects/NLogProjects/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace NLogProjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Nlog nlog = new Nlog();
+            int i = 1;
+            while (true)
+            {
+                nlog.Trace("Loop number= " + i);
+                Console.WriteLine("Enter value of a: ");
+                int a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter value of b: ");
+                int b = Convert.ToInt32(Console.ReadLine());
+
+                Math m = new Math();
+                Console.WriteLine(m.DivideTwoNumbers(a,b));
+
+                i++;
+            }
+        }
+    }
+}
